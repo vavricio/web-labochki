febfb2881ae879856affa7c2a595e4ce8c22d51a
Vavricio <anna.vavrik2002@gmail.com>
2021-04-24 17:27:01 +0300

    added 4 lab

---
+++ 4_lab/django_lab/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from redis import Redis
+
+redis = Redis('localhost', port=6379)
+
+
+class KpiChans(models.Model):
+    name = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+    subscribers = models.CharField(max_length=100)
+
+    def as_dict(self):
+        return {
+            'name': self.name,
+            'link': self.link,
+            'subscribers': self.subscribers,
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**data)
